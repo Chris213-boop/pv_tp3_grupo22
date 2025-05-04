ec292179797f1934dbe40aadf9964b0f490e3bb6
Torres Pablo <eneporene25@gmail.com>
2025-05-04 20:50:51 -0300

    Torres Pablo Gabriel Incorpora punto 1,2,5 en Producto.jsx

--- src/assets/components/Producto.jsx
+++ src/assets/components/Producto.jsx
@@ -4,17 +4,40 @@ import App from '../../App';
 const productos = [
   { id: 1, nombre: 'Camisa', precio: 25 },
   { id: 2, nombre: 'Pantalón', precio: 40 },
-  { id: 3, nombre: 'Remera', precio:20  },
+  { id: 3, nombre: 'Remera', precio: 20  },
   { id: 4, nombre: 'Buzos', precio: 60 },
-  { id: 5, nombre: 'zapatos', precio: 59 },
+  { id: 5, nombre: 'Zapatos', precio: 59 },
   { id: 6, nombre: 'Camperas', precio: 65 },
   { id: 7, nombre: 'Gorras', precio: 15 },
-  { id: 7, nombre: 'Chombas', precio: 50 },
+  { id: 8, nombre: 'Chombas', precio: 50 },
+  { id: 9, nombre: 'Guantes', precio: 18 },
+  { id: 10, nombre: 'Bufandas', precio: 22 }
 ];
 
 function Productos() {
-  console.log("Lista de productos:");
-  console.log(productos);
+  // console.log("Lista de productos:");
+  // console.log(productos);
+
+//1 - Mostrar en consola cada producto en el formato: "Producto: [descripción] - Precio:
+//        $[precio]", usando forEach.
+
+productos.forEach((producto, index) => {
+  console.log("Producto",index + 1,": " , producto.nombre," - Precio: $" , producto.precio);
+});
+
+console.log("---------------------------------------------");
+
+// 2 - Crear un nuevo array con los productos cuyo precio sea mayor a $20, usando filter.
+const preciosMayores = productos.filter((productos => (productos.precio > 20)));
+    console.log(preciosMayores);
+
+//5 - Agregar un nuevo producto al final del array (por ejemplo, { descripcion: "Parlante
+//   Bluetooth", precio: 59000.90 }).
+productos.push({
+  id: productos.length +1,
+  nombre: "Parlante Bluetooth",
+  precio: 59000.90
+});
 
   return (
     <div>
